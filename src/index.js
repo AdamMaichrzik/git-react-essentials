181e21e8adbce288c259453667ba34c88ede96e8
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-03-26 14:18:16 +0100

    Adds unordered list to the website

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
- React.createElement("h1",
-  {style: {color: "blue"}},
-  "Heyyy everyone!"),
+<ul>
+  <li>Monday</li>
+  <li>Tuesday</li>
+  <li>Wednesday</li>
+</ul>,
   document.getElementById('root')
 );
