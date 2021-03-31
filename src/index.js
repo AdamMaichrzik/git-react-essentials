76cb412abba9a81d6b6db42db19118401d612e5f
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-03-31 14:38:36 +0200

    Destructuring arrays and objects

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const [,, light] = [
+  "boots",
+   "tent",
+    "headlamp"
+  ]; 
+console.log(light);
+
 ReactDOM.render(
-<App authorized={true}/>, 
+<App authorized={false}/>, 
   document.getElementById('root')
 );
